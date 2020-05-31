af0bb06a2a1aa1170f2bb1242555aa3e909023dc
Noland Seigler <nolandseigler@gmail.com>
2020-05-30 23:03:03 -0500

    working with bs4 object and tags

--- requests_example.py
+++ requests_example.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-webpage_response = requests.get('https://s3.amazonaws.com/codecademy-content/courses/beautifulsoup/shellter.html', "html.parser")
 
-soup = BeautifulSoup(webpage_response.content)
-print(soup)
+webpage_response = requests.get('https://s3.amazonaws.com/codecademy-content/courses/beautifulsoup/shellter.html')
+
+webpage = webpage_response.content
+soup = BeautifulSoup(webpage, "html.parser")
+
+print(soup.p)
+print(soup.p.string)
